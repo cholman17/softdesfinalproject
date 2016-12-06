42bf3d99dcd2a96b9a3e52fa85c2dd4c43b1edd8
Prabha Dublish <pdublish1@babson.edu>
2016-12-05 23:53:47 -0500

    Starting to incorporate scheduler

--- creatingwebsite.py
+++ creatingwebsite.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import fnmatch
 from audit import audit
+from main import main
 
 app=Flask(__name__)
 
@@ -13,6 +14,10 @@ def get_classes():
     response = request.form["response"].encode('utf-8')
     remaining = audit(response)
     return render_template('audit_results.html',response=remaining)
+
+def get_schedule():
+    schedule = main()
+    return render_template('create_schedule.html',response=schedule)
 
 if __name__ == '__main__':
     app.run()
